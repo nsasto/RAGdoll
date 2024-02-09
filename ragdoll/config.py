35dd581084186e02d60603b542bb319b76515fd8
Nathan Sasto <Nathan.Sasto@uk.ey.com>
2024-02-09 16:48:39 +0000

    lowered query count to avoid hitting quota limit during testing

--- ragdoll/config.py
+++ ragdoll/config.py
@@ -1,8 +1,8 @@
 
 ragdoll_config = {
     'enable_logging': False,
-    'max_search_results_per_query': 5,
-    'alternative_query_term_count': 5,
+    'max_search_results_per_query': 2,
+    'alternative_query_term_count': 2,
     'max_workers': 3,
     'embeddings' : 'OpenAIEmbeddings',
     'vector_store': 'FAISS'
