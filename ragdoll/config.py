8cf316e3e14d6306bcf2d9159205c8a4a56e3b1f
Nathan Sasto <Nathan.Sasto@uk.ey.com>
2024-02-12 12:32:22 +0000

    updated config

--- ragdoll/config.py
+++ ragdoll/config.py
@@ -1,30 +1,38 @@
 class Config:
     """Config class for Ragdoll."""
-    
+
     _RETRIEVERS = ['BASE','MULTIQUERY']
     _LLM_PROVIDERS = ['OpenAI']
     _EMBEDDINGS_MODELS = ['OpenAIEmbeddings']
     _VECTOR_DB = ['FAISS','Chroma']
+    _DEFAULT_CONFIG = {
+        "vector_db":"FAISS",
+         "retriever": "BASE",
+         "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
+         "max_search_results_per_query" : 3,
+         "alternative_query_term_count" : 2,
+         "max_workers" : 3,
+         "llm": "OpenAI",
+         "embeddings": "OpenAIEmbeddings",
+         "enable_logging": False
+    }
 
     def __init__(self, config_settings=None):
         """Initialize the config class."""
-
-        self.vector_db = "FAISS"
-        self.retriever = "BASE"
-        self.user_agent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                           "(KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0")
-        self.max_search_results_per_query = 3
-        self.alternative_query_term_count = 2
-        self.max_workers = 3
-        self.llm = 'OpenAI'
-        self.embeddings = "OpenAIEmbeddings"
-        self.enable_logging = False
         self.load_config(config_settings)
 
     def load_config(self, config_settings=None) -> None:
         """Load the config file."""
+ 
+        for key, value in self._DEFAULT_CONFIG.items():
+            self.__dict__[key] = value
+
         if config_settings is None:
             return None
-
+        
         for key, value in config_settings.items():
             self.__dict__[key] = value
+    
+    def get_config(self):
+        """Get the current configuration."""
+        return self.__dict__
