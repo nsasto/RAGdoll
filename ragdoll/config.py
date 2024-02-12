670f4c81aa3347a98cbf840985b20d7dc6015298
Nathan Sasto <Nathan.Sasto@uk.ey.com>
2024-02-12 10:47:09 +0000

    evolved config to it's own class

--- ragdoll/config.py
+++ ragdoll/config.py
@@ -1,9 +1,28 @@
+class Config:
+    """Config class for GPT Researcher."""
+    _RETRIEVERS = ['BASE','MULTIQUERY']
+    _LLM_PROVIDERS = ['OpenAI']
+    _EMBEDDINGS_MODELS = ['OpenAIEmbeddings']
+    _VECTOR_DB = ['FAISS','Chroma']
 
-ragdoll_config = {
-    'enable_logging': False,
-    'max_search_results_per_query': 2,
-    'alternative_query_term_count': 2,
-    'max_workers': 3,
-    'embeddings' : 'OpenAIEmbeddings',
-    'vector_store': 'FAISS'
-}
+    def __init__(self, config_settings=None):
+        """Initialize the config class."""
+
+        self.vector_db = "FAISS"
+        self.retriever = "BASE"
+        self.user_agent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                           "(KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0")
+        self.max_search_results_per_query = 3
+        self.alternative_query_term_count = 2
+        self.max_workers = 3
+        self.embeddings = "OpenAIEmbeddings"
+        self.enable_logging = False
+        self.load_config(config_settings)
+
+    def load_config(self, config_settings=None) -> None:
+        """Load the config file."""
+        if config_settings is None:
+            return None
+
+        for key, value in config_settings.items():
+            self.__dict__[key] = value
