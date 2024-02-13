0810d541c6efa83e538e84aaa157f4b0421bbe91
Nathan Sasto <Nathan.Sasto@uk.ey.com>
2024-02-13 12:02:42 +0000

    added contextual compression pipeline

--- ragdoll/retriever.py
+++ ragdoll/retriever.py
@@ -1,11 +1,18 @@
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import EmbeddingsFilter
+from langchain.retrievers.document_compressors import DocumentCompressorPipeline
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_transformers import EmbeddingsRedundantFilter
+
+import numpy as np
 
 import logging
 
 from .config import Config
 from .multi_query import MultiQueryRetriever
-
+from .helpers import dotDict
 
 class RagdollRetriever:
     def __init__(self, config={}):
@@ -54,7 +61,6 @@ class RagdollRetriever:
                 "Embeddings model not specified. Set this in the config dictionary"
             )
 
-        self.embeddings = embeddings
         return embeddings
 
     def get_db(self, documents=None, vector_store=None, embeddings=None):
@@ -101,9 +107,33 @@ class RagdollRetriever:
         self.db = vectordb
         return vectordb
 
-    def _get_mq_retriever(self, retriever):
+    def get_mq_retriever(self, documents=None, db=None):
+        """
+        Returns a multi query retriever object based on the specified vector store.
+
+        Args:
+            documents (list): List of documents to be used for creating the retriever or
+            db (vetor database): a populated vector db for conversion to a retriever
+
+        Returns:
+            retriever: The retriever object based on the specified vector store.
+                       If the vector store already exists, will convert it to a langchain retriever
+                       If documents are provided (and no db), a new db will be created
+
+        Raises:
+            TypeError: If the vector store is not specified in the config dictionary.
+        """
+        self.logger.debug("retrieving multi query document retriever")
+        if db == None:
+            vector_db = self.get_db(documents)
+        else:
+            vector_db = db
+
+        retriever = vector_db.as_retriever()
+        self.logger.debug("Remember that the multi query retriever will incur additional calls to your LLM")
         return MultiQueryRetriever.from_llm(retriever=retriever, llm=self.llm)
-        
+
+
     def get_retriever(self, documents=None, db=None):
         """
         Returns a retriever object based on the specified vector store.
@@ -122,22 +152,70 @@ class RagdollRetriever:
         """
         self.logger.debug("retrieving document retriever")
         if db == None:
-            vector_db = self.get_db() if documents is None else self.get_db(documents)
+            vector_db = self.get_db(documents)
         else:
             vector_db = db
 
         retriever = vector_db.as_retriever()
         
-        #if base retriever type is set as a multi query retriever, then get that
-        if (self.cfg.base_retriever=='MULTI_QUERY'):
-            self.logger.debug("retrieving multi query document retriever")
-            self.retriever = self._get_mq_retriever(retriever)
-        else:
-            self.logger.debug("retrieving base document retriever")
-            self.retriever = retriever
+        return retriever
 
-        return self.retriever
+    def _default_compressor_config(self):
+        return {
+            "use_embeddings_filter":True, 
+            "use_splitter":True, 
+            "use_redundant_filter":True, 
+            "use_relevant_filter":True,
+            "embeddings":None,
+            "similarity_threshold":0.76, #embeddings filter settings
+            "chunk_size":500,   #text filter settings
+            "chunk_overlap":0, 
+            "separator":". ",
+        }
 
+    def get_compression_retriever(self, base_retriever, compressor_config={}):
+        crcfg=self._default_compressor_config()
+        for key, value in compressor_config.items():
+            crcfg.__dict__[key] = value
+        
+        crcfg = dotDict(crcfg)
+
+        #embeddings filter
+        embeddings = self.get_embeddings() if crcfg.embeddings is None else crcfg.embeddings
+        embeddings_filter = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=crcfg.similarity_threshold)
+        # Split documents into chunks of half size, 500 characters, with no characters overlap.
+        splitter = CharacterTextSplitter(chunk_size=crcfg.chunk_size, chunk_overlap=crcfg.chunk_overlap, separator=crcfg.separator)
+
+        # Remove redundant chunks, based on cosine similarity of embeddings.
+        redundant_filter = EmbeddingsRedundantFilter(embeddings=embeddings)
+
+        # Remove irrelevant chunks, based on cosine similarity of embeddings.
+        relevant_filter = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=crcfg.similarity_threshold)
+
+        # boolean vector 
+        config_switches = np.array([crcfg.use_embeddings_filter, crcfg.use_splitter, crcfg.use_redundant_filter, crcfg.use_relevant_filter])
+
+        # list of objects
+        compression_objects = [embeddings_filter, splitter, redundant_filter, relevant_filter]
+        
+        if len(compression_objects) == 0:
+            raise ValueError("No compression objects were selected")
+        
+        compression_objects_log = ['embeddings_filter', 'splitter', 'redundant_filter', 'relevant_filter']
+
+        log = [obj for flag, obj in zip(config_switches, compression_objects_log) if flag]
+        self.logger.debug(f"Compression object pipeline: {' ➤ '.join(log)}")
+
+        pipeline = [obj for flag, obj in zip(config_switches, compression_objects) if flag]
+        
+        pipeline_compressor = DocumentCompressorPipeline(
+            transformers=pipeline
+        )
+        compression_retriever = ContextualCompressionRetriever(
+            base_compressor=pipeline_compressor, base_retriever=base_retriever
+        )
+        
+        return compression_retriever
 
 if __name__ == "main":
     print("RAGdoll Retriever")
