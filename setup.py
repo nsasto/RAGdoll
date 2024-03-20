b20e66cf8140bd077d98935fd9b3d06e72e2076d
Nathan Sasto <nsasto@gmail.com>
2024-03-20 08:43:51 +0000

    setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='RAGdoll',
+    version='1.0.0',
+    description='A set of helper classes that abstract some of the more common tasks of a typical RAG process including document loading/web scraping.',
+    author='Nathan Sasto',
+    packages=find_packages(),
+    install_requires=requirements,
+)
