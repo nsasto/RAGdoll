3a232fe696b84adf5d9dab3e3a55665a0cf6c5ed
nsasto <nsasto@gmail.com>
2025-04-04 17:13:22 +0000

    ingestion and test

---
+++ tests/test_ingestion_service.py
@@ -0,0 +1,27 @@
+import unittest
+from ragdoll.ingestion.ingestion_service import IngestionService
+
+class TestIngestionService(unittest.TestCase):
+
+    def test_ingestion_service(self):
+        ingestion_service = IngestionService()
+
+        test_sources = [
+            "https://www.example.com",
+            "test_docs/dummy.txt",
+            "test_docs/ukpga_20070003_en.pdf",
+          ]
+
+        ingested_documents = ingestion_service.ingest_documents(test_sources)
+
+        self.assertIsInstance(ingested_documents, list)
+        self.assertTrue(len(ingested_documents) > 0)
+
+        for document in ingested_documents:
+            self.assertTrue(document.page_content != "")
+
+            if document.metadata.get("source") == "tests/dummy.txt":
+                self.assertTrue("This is a dummy text file for testing." in document.page_content)
+
+if __name__ == "__main__":
+    unittest.main()
